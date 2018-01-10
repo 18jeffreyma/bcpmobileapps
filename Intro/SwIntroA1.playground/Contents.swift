407f70570231f2dbdf973da502057cbc0891610f
Jeffrey Ma <jma@Jeffreys-Mac.local>
2018-01-09 20:16:15 -0800

    First commit

---
+++ Intro/SwIntroA1.playground/Contents.swift
@@ -0,0 +1,39 @@
+// Greeting Section
+
+import UIKit
+
+let myName = "Jeffrey"
+// myName = "Not Jeffrey"
+
+var greeting = "Ayy waddup "
+greeting += myName
+
+print(greeting)
+
+// Loops Section
+
+for index in 1...10 {
+    print(index)
+}
+
+for index in (1...10).reversed() {
+    print(index)
+}
+
+// Countdown Section
+
+let startValue = 25
+print("Countdown")
+for value in (0...startValue).reversed() {
+    print(value)
+}
+print("BOOM")
+
+
+
+
+
+
+
+
+
