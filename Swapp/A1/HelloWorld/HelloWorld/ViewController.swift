0142302d85145ca17e4428f8694d845e924b53ce
Ma <jeffrey.ma18@mbpodonl003-2.local>
2018-01-16 09:52:16 -0800

    Finished SwAppA1 and also added screenshot

---
+++ Swapp/A1/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ma, Jeffrey '18 on 1/16/18.
+//  Copyright © 2018 Ma, Jeffrey '18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var centeredText: UILabel!
+    
+    @IBOutlet weak var datetime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        centeredText.text = "ayy lmao"
+        centeredText.font = UIFont(name: "Chalkduster", size: 50)
+        centeredText.textColor = UIColor.blue
+        
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        
+        datetime.text = String(hour) + ":" + String(minutes)
+        datetime.font = UIFont(name: "Chalkduster", size: 50)
+        datetime.textColor = UIColor.red
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
