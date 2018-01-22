a66bd18802f0dbf28b502803678e720524d8b327
Ma <jeffrey.ma18@mbpodonl003-2.local>
2018-01-22 09:18:15 -0800

    updated background color for quote generator app

--- Swapp/A3/QuoteGenerator/QuoteGenerator/ViewController.swift
+++ Swapp/A3/QuoteGenerator/QuoteGenerator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var theLabel: UILabel!
     
+    
     let einsteinQuotes: [String] = ["There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.", "We still do not know one-thousandth of one percent of what nature has revealed to us.", "He who can no longer pause to wonder and stand rapt in awe, is as good as dead his eyes are closed.", "It is the theory that decides what we can observe.", "We should take care not to make the intellect our god it has, of course, powerful muscles, but no personality."]
     let ghandiQuotes: [String] = ["Happiness is when what you think, what you say, and what you do are in harmony.", "Where there is love there is life.", "Strength does not come from physical capacity. It comes from an indomitable will.", "The future depends on what we do in the present.", "Action expresses priorities."]
     let seussQuotes: [String] = ["Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.", "The more that you read, the more things you will know. The more that you learn, the more places you'll go.", "Don't cry because it's over. Smile because it happened.", "Today you are you! That is truer than true! There is no one alive who is you-er than you!", "Unless someone like you cares a whole awful lot, nothing is going to get better. It's not."]
@@ -21,6 +22,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
+
     }
 
     override func didReceiveMemoryWarning() {
