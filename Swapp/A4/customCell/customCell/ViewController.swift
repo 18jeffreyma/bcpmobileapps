e9c83c03f7c577ba4e310efa9b404cf34208c235
Ma <jeffrey.ma18@mbpodonl003-2.local>
2018-01-25 14:45:27 -0800

    Finished TableView App

--- Swapp/A4/customCell/customCell/ViewController.swift
+++ Swapp/A4/customCell/customCell/ViewController.swift
@@ -9,14 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return (list.count)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+        
+        cell.myImage.image = UIImage(named: list[indexPath.row] + ".jpg")
+        cell.myLabel.text = list[indexPath.row]
+        
+        return (cell)
     }
+    
+    
+    let list = ["Brad Lindemann", "Jeffrey Ma", "Will Portman", "Alexander Knappen", "Joshua Holden", "Phillip Si"]
+    
+    
+
     
 
     override func viewDidLoad() {
---
+++ Swapp/A4/customCell/customCell/ViewControllerTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerTableViewCell.swift
+//  customCell
+//
+//  Created by Ma, Jeffrey '18 on 1/25/18.
+//  Copyright © 2018 Ma, Jeffrey '18. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
