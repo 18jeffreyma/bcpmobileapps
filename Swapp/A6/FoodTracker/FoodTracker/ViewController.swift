07f2aa12b044cbded604f1f160cb1eb9df0e89e5
Jeffrey Ma <jeffrey.ma18@bcp.org>
2018-01-31 13:54:15 -0800

    Started 6th app: Real Table View

---
+++ Swapp/A6/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ma, Jeffrey '18 on 1/31/18.
+//  Copyright © 2018 Ma, Jeffrey '18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
